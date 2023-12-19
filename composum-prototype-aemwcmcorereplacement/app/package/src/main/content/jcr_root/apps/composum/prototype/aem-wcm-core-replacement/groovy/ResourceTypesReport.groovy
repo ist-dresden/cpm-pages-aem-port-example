76858368d95b238a9813165b7e0c781b2be7c175
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-12-19 15:50:11 +0100

    start migration exploration

---
+++ composum-prototype-aemwcmcorereplacement/app/package/src/main/content/jcr_root/apps/composum/prototype/aem-wcm-core-replacement/groovy/ResourceTypesReport.groovy
@@ -0,0 +1,45 @@
+import org.apache.sling.api.resource.Resource;
+
+void checkPath(String path) {
+    Map<String, Integer> primtypes = new TreeMap();
+    Map<String, Integer> mixins = new TreeMap();
+    Map<String, Integer> restypes = new TreeMap();
+
+    resourceResolver.findResources(
+            "/jcr:root" + path + "//*",
+            "xpath"
+    ).each { Resource r ->
+        String primtype = r.getValueMap().get("jcr:primaryType")
+        primtypes.put(primtype, primtypes.getOrDefault(primtype, 0) + 1)
+
+        String[] mix = r.getValueMap().get("jcr:mixinTypes", new String[0])
+        mix.each {m ->
+            mixins.put(m, mixins.getOrDefault(m, 0) + 1)
+        }
+
+        String restype = r.getValueMap().get("sling:resourceType", "")
+        restypes.put(restype, restypes.getOrDefault(restype, 0) + 1)
+    }
+
+    println "\n\nPrimtypes:"
+    primtypes.entrySet().each {entry ->
+        println entry.getKey() + "\t" + entry.getValue()
+    }
+
+    println "\n\nMixins:"
+    mixins.entrySet().each {entry ->
+        println entry.getKey() + "\t" + entry.getValue()
+    }
+
+    println "\n\nsling:resourceType sorted by key:"
+    restypes.entrySet().each {entry ->
+        println entry.getKey() + "\t" + entry.getValue()
+    }
+
+    println "\n\nsling:resourceType sorted by count:"
+    restypes.entrySet().sort { a, b -> b.getValue() <=> a.getValue() }.each {entry ->
+        println entry.getKey() + "\t" + entry.getValue()
+    }
+}
+
+checkPath("/content/wknd")
---
+++ documentation/wkndmigration.md
@@ -0,0 +1,14 @@
+# Partial Migration of the AEM WKND project to Composum Pages
+
+This document describes the steps taken to migrate the [AEM WKND project](https://github.com/adobe/aem-guides-wknd) to
+Composum Pages.
+
+## Steps
+
+1. Create package with /content/wknd, /content/dam/wknd, /conf/wknd to import into a Composum Pages
+2. Import that package into a Composum Pages test instance. Note: the additional AEM nodetypes are automatically
+   registered by the package import.
+
+## Analysis of component structure
+
+(compare [component structure analysis](https://stoerr.github.io/til/AdobeAEM/AnalyzeSite.html))
